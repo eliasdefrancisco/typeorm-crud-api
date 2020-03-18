a20929085221e1e79166253ba7a80f0476095919
Elias de Francisco <elias@iMac-de-Elias.local>
2020-03-18 19:16:33 +0100

    typeorm basics

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import 'reflect-metadata'
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import userRoutes from './routes/user.routes'
+import { createConnection } from 'typeorm'
 
 const app = express()
+createConnection()
 
 // middlewares
 app.use(cors())
