25d984d901d5645eb9f105c1826af97ededdcaa5
Elias de Francisco <elias@iMac-de-Elias.local>
2020-03-18 19:54:55 +0100

    CRUD created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { getUsers } from "../controllers/user.controller";
+import { getUsers, createUser, getUser, updateUser, deleteUser } from "../controllers/user.controller";
 
 const router = Router()
 
 router.get('/users', getUsers)
-// router.get('/users/:id', )
-// router.post('/users', )
-// router.put('/users', )
-// router.delete('/users/:id', )
+router.get('/users/:id', getUser)
+router.post('/users', createUser)
+router.put('/users/:id', updateUser)
+router.delete('/users/:id', deleteUser)
 
 export default router
